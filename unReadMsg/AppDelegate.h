41e20e3cbaef7210309ca7f70560c3500960a316
xionghuai zhou <xh1billy@163.com>
2017-05-18 15:47:54 +0800

    Initial Commit

---
+++ unReadMsg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  unReadMsg
+//
+//  Created by BillyChou on 2017/5/18.
+//  Copyright © 2017年 Jane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ unReadMsg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  unReadMsg
+//
+//  Created by BillyChou on 2017/5/18.
+//  Copyright © 2017年 Jane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
